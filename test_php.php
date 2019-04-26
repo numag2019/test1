00723ff7968fc4d75dcde9c465b09af42380e0a8
theonobella <49910737+theonobella@users.noreply.github.com>
2019-04-26 09:48:12 +0200

    fichier test de tex

---
+++ test_php.php
@@ -0,0 +1,20 @@
+<!-- Cette page affiche un tableau des observations-->
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>
+		Oiseaux - Projet Techno. Web
+	</title>
+</head>
+
+<body>
+
+<?php
+$texte="Je sui un texte test et j'aime ça";
+echo $texte ;
+?>
+
+</div><!-- #global -->
+
+</body>
+</html>
